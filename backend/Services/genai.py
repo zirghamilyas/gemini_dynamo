86bfc5677aa3d266cf2e4a355c5dedfdaa38baec
Zirgham Ilyas <zirghamilyas@gmail.com>
2024-06-11 09:09:16 +0100

    solved the issue of Gemini API

--- backend/Services/genai.py
+++ backend/Services/genai.py
@@ -3,9 +3,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_vertexai import VertexAI
 from langchain.chains.summarize import load_summarize_chain
 
-from google.oauth2.service_account import Credentials
-import sys, os
-sys.path.append(os.path.abspath('../../'))
+# from google.oauth2.service_account import Credentials
+# import sys, os
+# sys.path.append(os.path.abspath('../'))
 
 class YoutubeProcessor:
     def __init__(self):
@@ -31,36 +31,32 @@ class YoutubeProcessor:
     
 
 class GeminiProcessor:
-    def __init__(self, model_name, project, credential_file_name):
-        import sys, os
-        root_dir = os.path.abspath('../../')
-        key_path = root_dir + '\\' + credential_file_name + '.json'
-        self.credentials = Credentials.from_service_account_file(key_path)
-        self.model = VertexAI(name=model_name, project= project, credentials= self.credentials)
+    def __init__(self, model_name, project):
+        # import sys, os
+        # root_dir = os.path.abspath('../')
+        # key_path = root_dir + '\\' + credential_file_name + '.json'
+        # self.credentials = Credentials.from_service_account_file(key_path)
+        self.model = VertexAI(model_name=model_name, project= project)
 
     def generate_document_summary(self, documents: list, **args):
-        chain_type = 'map_reducer' if len(documents) > 10 else 'stuff'
+        chain_type = 'map_reduce' if len(documents) > 10 else 'stuff'
         chain = load_summarize_chain(
-            chain_type,
             llm= self.model,
+            chain_type = chain_type,
             **args
         )
         return chain.run(documents)
     
 
-if __name__=='__main__':
-    credential_file_name = 'gemini-dynamo-credentials'
-    model_name ="gemini-1.5-pro-001"
-    project_id = 'gemini-dynamo-423808'
-    gemini_processor = GeminiProcessor(model_name = model_name, 
-                                       project = project_id,
-                                       credential_file_name = credential_file_name )
-    documents = "This is a text just to check my credentials are working fine and model is responding properly. I will generate proper results later, meanwhile just use this text and check the code."
-    gemini_processor.generate_document_summary(documents=documents)
-
-    
-    # import google.generativeai as genai
-
-    # model = genai.GenerativeModel('gemini-pro')
-    # response = model.generate_content("What is the meaning of life?")
-    # print(response.text)
+# if __name__=='__main__':
+#     credential_file_name = 'gemini-dynamo-credentials'
+#     model_name ="gemini-1.5-pro-001"
+#     project_id = 'gemini-dynamo-423808'
+#     genai_processor = GeminiProcessor(model_name = model_name, 
+#                                        project = project_id,
+#                                     #    credential_file_name = credential_file_name 
+#                                        )
+#     documents = "This is a text just to check my credentials are working fine and model is responding properly. I will generate proper results later, meanwhile just use this text and check the code."
+#     genai_processor.generate_document_summary(documents=documents)  
+#     result = gemini_processor.generate_document_summary(documents=documents)
+#     print(result)
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
 from fastapi.middleware.cors import CORSMiddleware
 
-from Services.genai import YoutubeProcessor
+from Services.genai import YoutubeProcessor, GeminiProcessor
 
 class VideoAnalysisRequest(BaseModel):
     youtube_link : HttpUrl
@@ -24,5 +24,8 @@ app.add_middleware(
 def analyze_video(request : VideoAnalysisRequest):
     processor = YoutubeProcessor()
     result = processor.retrieve_youtube_documents(str(request.youtube_link), verbose = True)
-    
-    return {'result' : result}
+    genai_processor = GeminiProcessor(model_name = 'gemini-pro', 
+                                      project = 'gemini-dynamo-423808')
+    summary = genai_processor.generate_document_summary(documents = result, 
+                                                        verbose = True)
+    return {'summary' : summary}
